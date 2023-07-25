fe148c8b978660d21ead420122749d7151bee5e4
jihyunniiii <jita1125@naver.com>
2023-07-25 19:26:56 +0900

    [feat] post list API에서 댓글 개수 보여주기

--- post/serializers.py
+++ post/serializers.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from .models import *
 
 class PostSerializer(serializers.ModelSerializer):
+    def get_comments(self, instance):
+        serializers = CommentSerializer(instance.comments, many=True)
+        return serializers.data
+
     class Meta:
         model = Post
         fields = '__all__'
-        read_only_field = ['id', 'created_at', 'updated_at']
+        read_only_field = ['id', 'created_at', 'updated_at', 'comments']
 
 class CommentSerializer(serializers.ModelSerializer):
     post = serializers.SerializerMethodField()
@@ -17,3 +21,14 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
         read_only_field = ['id', 'created_at', 'update_at', 'post']
+
+class PostListSerializer(serializers.ModelSerializer):
+    comments_cnt = serializers.SerializerMethodField()
+
+    def get_comments_cnt(self, instance):
+        return instance.comments.count()
+    
+    class Meta:
+        model = Post
+        fields = ['id','writer','created_at','updated_at','comments_cnt']
+        read_only_field = ['id', 'created_at', 'updated_at','comments_cnt']
--- post/views.py
+++ post/views.py
@@ -4,26 +4,28 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer, PostListSerializer
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PostListSerializer
+        return PostSerializer
 
 class CommentViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 class PostCommentViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-    def list(self, request, post_id=None):
-        post = get_object_or_404(Post, id=post_id)
-        queryset = self.filter_queryset(self.get_queryset().filter(post=post))
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        post = self.kwargs.get("post_id")
+        queryset = Comment.objects.filter(post_id = post)
+        return queryset
     
     def create(self, request, post_id=None):
         post = get_object_or_404(Post, id=post_id)
